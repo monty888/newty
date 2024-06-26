5b76205232c56cdf1ff8cb08c525688f41897023
monty <monty@monty888.com>
2024-06-26 16:26:36 +0100

    removed use of password dialog, now just a part of the accounts window

--- src/newty/gui/dialogs/account.py
+++ src/newty/gui/dialogs/account.py
@@ -107,7 +107,16 @@ class AccountManager(QWidget):
         super(AccountManager, self).__init__(*args, **kargs)
 
         self.create_gui()
-        self._key_store = self.get_sqlite_key_store(db_file=WORK_DIR + KEY_STORE_DB_FILE)
+
+        self._key_store = None
+        self._relist_accounts = True
+
+        async def get_key() -> str:
+            self._relist_accounts = True
+            return self._password_in.text()
+        self._key_enc = KeyDataEncrypter(get_key=get_key)
+        self._key_store = SQLiteKeyStore(file_name=WORK_DIR+ KEY_STORE_DB_FILE,
+                                         encrypter=self._key_enc)
 
         self.setFixedWidth(400)
         self.setFixedHeight(400)
@@ -125,6 +134,7 @@ class AccountManager(QWidget):
         self._layout = QVBoxLayout()
         self.setLayout(self._layout)
         self._tabs = QTabWidget()
+        self._tabs.currentChanged.connect(self._tabs_changed)
 
         tab1 = QWidget()
         tab1_layout = QVBoxLayout()
@@ -143,7 +153,8 @@ class AccountManager(QWidget):
         settings_con_layout.addWidget(file,0,1, alignment=Qt.AlignTop)
 
         password_lbl = QLabel('password')
-        password = QLineEdit()
+        self._password_in = password = QLineEdit()
+        self._password_in.textChanged.connect(self._password_changed)
         password_lbl.setBuddy(file)
         settings_con_layout.addWidget(password_lbl, 1, 0, alignment=Qt.AlignTop)
         settings_con_layout.addWidget(password, 1, 1, alignment=Qt.AlignTop)
@@ -156,26 +167,38 @@ class AccountManager(QWidget):
         self._layout.addWidget(self._tabs)
 
 
-        tab2 = QWidget()
+        self._tab2 = QWidget()
         tab2_layout = QVBoxLayout()
-        tab2.setLayout(tab2_layout)
+        self._tab2.setLayout(tab2_layout)
+
+        self._tab2_main_con = QWidget()
+        tab2_main_con_layout = QVBoxLayout()
+        self._tab2_main_con.setLayout(tab2_main_con_layout)
+        tab2_layout.addWidget(self._tab2_main_con)
+        self._tab2_err_con = QWidget()
+        tab2_err_con_layout = QVBoxLayout()
+        self._tab2_err_con.setLayout(tab2_err_con_layout)
+        tab2_layout.addWidget(self._tab2_err_con)
 
         sel_view_con = QWidget()
         sel_view_con_layout = QHBoxLayout()
         sel_view_con.setLayout(sel_view_con_layout)
         sel_view_con_layout.addWidget(QLabel('about current selection'))
-        tab2_layout.addWidget(sel_view_con)
+        tab2_main_con_layout.addWidget(sel_view_con)
 
         self._acc_list = QListWidget()
         self._acc_list.itemSelectionChanged.connect(self.account_select)
-        tab2_layout.addWidget(self._acc_list)
+        tab2_main_con_layout.addWidget(self._acc_list)
 
         self._new_btn = QPushButton('New')
         self._new_btn.clicked.connect(self.new_account)
-        tab2_layout.addWidget(self._new_btn)
+        tab2_main_con_layout.addWidget(self._new_btn)
+
+        self._tab2_err = QLabel()
+        tab2_err_con_layout.addWidget(self._tab2_err)
 
 
-        self._tabs.addTab(tab2, 'accounts')
+        self._tabs.addTab(self._tab2, 'accounts')
         # self._url_text = QLineEdit(placeholderText='something')
         self._layout.addWidget(self._tabs)
 
@@ -193,23 +216,50 @@ class AccountManager(QWidget):
         else:
             print('no selection')
 
+    def _password_changed(self):
+        self._key_enc.clear_key()
+
+    @asyncSlot()
+    async def _tabs_changed(self, idx):
+        if idx == 1:
+            await self._update_account_list()
+
+    async def _update_account_list(self):
+        try:
+            if self._relist_accounts:
+                self._acc_list.clear()
+                accounts = await self._key_store.select()
+                c_acc: NamedKeys
+                for c_acc in accounts:
+                    item = QListWidgetItem(util_funcs.str_tails(c_acc.public_key_hex()).ljust(20) + c_acc.name.ljust(44))
+                    item.setData(Qt.UserRole, c_acc)
+                    self._acc_list.addItem(item)
+            self._tab2_main_con.setDisabled(False)
+            self._tab2_err_con.setVisible(False)
+        except Exception as e:
+            self._tab2_main_con.setDisabled(True)
+            self._show_tab2_err('problem decrypting key store, bad password?')
+
+    def _show_tab2_err(self, txt: str):
+        self._tab2_err_con.setVisible(True)
+        self._tab2_err.setText(txt)
+
     def get_sqlite_key_store(self, db_file: str, password: str = None):
         # human alias to keys
         # keystore for user key aliases
         # TMP - borrowed from terminal - obvs we want to show a dialog
         async def get_key() -> str:
-            ret = password
-            if password is None:
-                self.setDisabled(True)
-                self._password_dialog.setEnabled(True)
-                if await self._password_dialog.ashow():
-                    ret = self._password_dialog.password
-                else:
-                    ret = ''
-                self.setDisabled(False)
-                return ret
-
-            return ret
+            # ret = password
+            # if password is None:
+            #     self.setDisabled(True)
+            #     self._password_dialog.setEnabled(True)
+            #     if await self._password_dialog.ashow():
+            #         ret = self._password_dialog.password
+            #     else:
+            #         ret = ''
+            #     self.setDisabled(False)
+            #     return ret
+            return self._password_in.text()
 
         key_enc = KeyDataEncrypter(get_key=get_key)
         return SQLiteKeyStore(file_name=db_file,
@@ -217,12 +267,7 @@ class AccountManager(QWidget):
 
     async def ashow(self):
         try:
-            accounts = await self._key_store.select()
-            c_acc: NamedKeys
-            for c_acc in accounts:
-                item = QListWidgetItem(util_funcs.str_tails(c_acc.public_key_hex()).ljust(20) + c_acc.name.ljust(44))
-                item.setData(Qt.UserRole, c_acc)
-                self._acc_list.addItem(item)
+
 
             while self.isVisible():
                 # print('runnming')
